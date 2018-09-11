a10bce6b3b9ea068b97d31de67cd90a2faf2c287
070519960 <lucashenri56@gmail.com>
2018-09-11 20:20:19 -0300

    adicionando evento

--- events/models.py
+++ events/models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 
 class Event(models.Model):
-
-    priority_list = (
+    priorities_list = (
         ('0', 'Sem prioridade'),
         ('1', 'Normal'),
         ('2', 'Urgente'),
         ('3', 'Muito Urgente'),
         ('4', 'Ultra mega Hiper Urgente')
+    )
     date = models.DateTimeField()
     event = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=priorities_list)
 
-
-    def__str__(self):
+    def __str__(self):
        return self.event
